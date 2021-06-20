758232102c391f4dc63be0e9e7576c94b76a891f
Willian Prado <willian_prado@hotmail.com>
2021-06-20 15:56:24 -0300

    Resolve o Requisito 1 - Crie testes para uma função assíncrona

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,5 +1,5 @@
-const assert = require('assert');
-// const answerPhone = require('../src/asyncJest');
+// const assert = require('assert');
+const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
@@ -11,12 +11,17 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('o retorno do telefonema', () => {
-  test('atende', () => {
-    // assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('atende', async () => {
+    const response = await answerPhone(true);
+    expect.assertions(1);
+    expect(response).toBe('Oi!');
   });
-  test('ocupado', () => {
-    // assert.fail();
-    // Insira seu teste assíncrono aqui
+  test('ocupado', async () => {
+    expect.assertions(1);
+    try {
+      await answerPhone(false);
+    } catch (error) {
+      expect(error).toEqual(new Error('Infelizmente não podemos atender...'));
+    }
   });
 });

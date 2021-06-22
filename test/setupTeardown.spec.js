b3006f9a8e411e05abc646b2f3b694da3cd028ad
Willian Prado <willian_prado@hotmail.com>
2021-06-22 00:41:20 -0300

    Resolve o Requisito 4 - Crie funções no arquivo test/setupTeardown.spec.js

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -21,6 +21,7 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
+  beforeEach(adventure.randomAttack);
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);

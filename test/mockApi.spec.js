77f40a8f1509daf3d1811d8d748a90bb0b667e26
Willian Prado <willian_prado@hotmail.com>
2021-06-21 21:51:11 -0300

    Resolve o Requisito 3 - Crie um Mock para o retorno da API

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -24,6 +24,21 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+  const mockUser = {
+    gender: 'male',
+    name: { title: 'Mr', first: 'Antônio', last: 'Britto' },
+    location: {
+      country: 'Brazil',
+    },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  };
+
+  // Feito com ajuda de: https://jestjs.io/pt-BR/docs/mock-functions
+  api.fetchURL = jest.fn(async () => mockUser);
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
